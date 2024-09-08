64780074f11271398cf8381ce072a3eabab40afa
idkcode68 <irafsan68@gmail.com>
2024-09-08 07:41:28 +0600

    Finalization almost done

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,14 @@
 from flask import Flask, redirect, render_template,flash,request
+from flask.globals import request, session
 from flask_sqlalchemy import SQLAlchemy 
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_required, logout_user, login_user, LoginManager, current_user
 from sqlalchemy import text
-from flask import url_for
 import json 
-from flask import session
+from flask.helpers import url_for
 
-from flask_mail import Mail
+# from flask_mail import Mail
 
 
 # I am getting error like this..AttributeError: 'Engine' object has no attribute 'execute' .  I am using SQLalchemy  2.0.30 version. I googled and found out that  in the new version of SqlAlchemy execute method is not supported any more. Please help me how to correct this.
@@ -19,19 +19,19 @@ app = Flask(__name__)
 app.secret_key = "qwerty"
 
 
-with open('config.json', 'r') as c:
-    params=json.load(c)["params"]
+# with open('config.json', 'r') as c:
+#     params=json.load(c)["params"]
 
 
 
-app.config.update(
-    MAIL_SERVER='smtp.gmail.com',
-    MAIL_PORT='465',
-    MAIL_USE_SSL=True,
-    MAIL_USERNAME=params['gmail-user'],
-    MAIL_PASSWORD=params['gmail-password']
-)
-mail = Mail(app)
+# app.config.update(
+#     MAIL_SERVER='smtp.gmail.com',
+#     MAIL_PORT='465',
+#     MAIL_USE_SSL=True,
+#     MAIL_USERNAME=params['gmail-user'],
+#     MAIL_PASSWORD=params['gmail-password']
+# )
+# mail = Mail(app)
 
 
 # this is for getting the unique user access
@@ -76,6 +76,21 @@ class Artistdata(db.Model):
     records = db.Column(db.Integer)
     label = db.Column(db.String(100))
 
+class Trackallot(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(20),unique=True)
+    genretype=db.Column(db.String(100))
+    Acode=db.Column(db.String(20))
+    duration=db.Column(db.Integer)
+    albumname=db.Column(db.String(100))
+    albumlabel=db.Column(db.String(100))
+    releasedate = db.Column(db.Date)
+
+
+
+
+
+
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -88,56 +103,45 @@ def usersignup():
 def userlogin():
     return render_template("userlogin.html")
 
-@app.route('/signup', methods=['POST', 'GET'])
+@app.route('/signup',methods=['POST','GET'])
 def signup():
-    if request.method == "POST":
-        id = request.form.get('id')
-        name = request.form.get('name')
-        email = request.form.get('email')
-        dob = request.form.get('dob')
-        encpassword = generate_password_hash(dob)
-        user=User.query.filter_by(id=id).first()
+    if request.method=="POST":
+        name=request.form.get('name')
+        email=request.form.get('email')
+        dob=request.form.get('dob')
+        user=User.query.filter_by(name=name).first()
         emailUser=User.query.filter_by(email=email).first()
-        if User or emailUser:
-            flash("Email or ID is already taken", "warning")
+        if user or emailUser:
+            flash("Email is already taken","warning")
             return render_template("usersignup.html")
-        
-        # eikhane hoilo youtube ee dekhsilam, db.engine.execute(f"INERT INTO 'table_name'('column1', 'column2',...) VALUES('{value1}','{value2}',.....) ")......... eitai problem amar sql er version ta updated and this does not work anymore pore chatgpt kore nicher tar moton kore likhsi but problem hoilo je eita run kore but mysql ee registered hoi na ekdom ee
+        new_user=User(name=name,email=email,dob=dob)
+        db.session.add(new_user)
+        db.session.commit()
+                
+        flash("SignUp Success Please Login","success")
+        return render_template("userlogin.html")
 
-        stmt = text("INSERT INTO User (id,name, email, dob) VALUES (:id, :name, :email, :dob)")
-        with db.engine.connect() as conn:
-            conn.execute(stmt, {"Acode": Acode, "Aname": Aname, "genre" : genre, "records": records, "label": label})
-            conn.execute(stmt, {"id": id, "name": name, "email": email, "dob": encpassword})
-        user1=User.query.filter_by(id=id).first()
-        if user1 and check_password_hash(user1.dob,dob):
-            login_user(user)
-            flash("login success", "info")
-            return render_template("index.html")
-        flash("SignIn Success", "success")
-        return render_template("index.html")
-    
-    return render_template("/usersignup.html")
+    return render_template("usersignup.html")
     # eikhane thik eki ee bhabe flash use kore amader message ta dibo, then dewar por oita amader index or home ee redirect hobe (index ee dewa tai beshi better)
 
 
 
-@app.route('/login', methods=['POST', 'GET'])
+@app.route('/login',methods=['POST','GET'])
 def login():
-    if request.method == "POST":
-        id = request.form.get('id')
-        dob = request.form.get('dob')
-        user=User.query.filter_by(id=id).first()
-
-        if user and check_password_hash(user.dob,dob):
+    if request.method=="POST":
+        name=request.form.get('name')
+        dob=request.form.get('dob')
+        user=User.query.filter_by(name=name).first()
+        if user and user.dob==dob:
             login_user(user)
-            flash("login success", "info")
+            flash("Login Success","info")
             return render_template("index.html")
         else:
-            flash("Inappropriate Credentials", "danger")
+            flash("Invalid Credentials","danger")
             return render_template("userlogin.html")
-     
-    
-    return render_template("/userlogin.html")
+
+
+    return render_template("userlogin.html")
 
 
 @app.route('/artistlogin', methods=['POST', 'GET'])
@@ -147,7 +151,7 @@ def artistlogin():
         password = request.form.get('password')
         user=Artistuser.query.filter_by(email=email).first()
 
-        if user and check_password_hash(user.password,password):
+        if user and user.password==password:
             login_user(user)
             flash("login success", "info")
             return render_template("index.html")
@@ -163,7 +167,7 @@ def admin():
     if request.method == "POST":
         username = request.form.get('username')
         password = request.form.get('password')
-        if(username==params['user'] and password==params['password']):
+        if(username=="admin" and password=="admin"):
             session['user'] = username
             flash("Login Success", "info")
             return render_template("addArtistUser.html")
@@ -183,40 +187,38 @@ def logout():
 # eikhane login korte gele kintu jhamela hobe aagei, dekhabe login hobe na ei shei, **eitar solution hoilo google er 2 step verification bondho kora lagbe, bondho kore tarpor try korle tokhon hobe
 # Rename the function to avoid name collision with the model
 @app.route('/addArtistUser', methods=['POST', 'GET'])
-def add_artist_user():
+def artistuser():
     
-    if 'user' in session and session['user'] == params['user']:
+    if 'user' in session and session['user']=="admin":
         if request.method == "POST":
             Acode = request.form.get('Acode')
             email = request.form.get('email')
             password = request.form.get('password')
-            encpassword = generate_password_hash(password)
+            # encpassword = generate_password_hash(password)
             Acode=Acode.upper()
-
             # Use the model to query the database
             emailUser = Artistuser.query.filter_by(email=email).first()
             if emailUser:
                 flash("Email is already taken", "warning")
-                return render_template("addArtistUser.html")
+                
+            query=Artistuser(Acode=Acode,email=email,password=password)
+            db.session.add(query)
+            db.session.commit()
             
-            stmt = text("INSERT INTO artistuser (Acode, email, password) VALUES (:Acode, :email, :password)")
-            with db.engine.connect() as conn:
-                conn.execute(stmt, {"Acode": Acode, "email": email, "password": encpassword})
-            
-            mail.send_message(
-                'Music Management System',
-                sender=params['gmail-user'],
-                recipients=[email],
-                body=f"Welcome thank you for choosing us\nYour Credentials Are:\nEmail Address: {email}\nPassword: {password}\n\nHospital Code {Acode}\n Do not share your information\n\n\n Thanks"
-            )
+            # mail.send_message(
+            #     'Music Management System',
+            #     sender=params['gmail-user'],
+            #     recipients=[email],
+            #     body=f"Welcome thank you for choosing us\nYour Credentials Are:\nEmail Address: {email}\nPassword: {password}\n\nHospital Code {Acode}\n Do not share your information\n\n\n Thanks"
+            # )
             
             flash("Data Inserted", "warning")
             return render_template("addArtistUser.html")
         
-        return render_template("addArtistUser.html")  # Handle GET request
+        
     else:
         flash("Login and try Again", "warning")
-        return redirect('/admin')
+        return render_template("addArtistUser.html")
 
 
 
@@ -225,15 +227,16 @@ def add_artist_user():
 
 # testing whether db is connected or not
 # pore eita try korsi, its actually not connected at all
+# testing wheather db is connected or not  
 @app.route("/test")
 def test():
     try:
-        a = Test.query.all()
+        a=Test.query.all()
         print(a)
-        return f'MY DATABASE IS CONNECTED{a.name}'
+        return f'MY DATABASE IS CONNECTED'
     except Exception as e:
         print(e)
-        return f'MY DATABASE IS NOT CONNECTED{e}'
+        return f'MY DATABASE IS NOT CONNECTED {e}'
 
 
 
@@ -270,14 +273,14 @@ def addartistinfo():
                 flash("Data is already Present you can update it", "primary")
                 return render_template("artistdata.html")  
             if auser:
-                stmt = text("INSERT INTO artistdata (Acode, Aname, Genre, Records, Label) VALUES (:Acode, :Aname, :genre, :records, :label)")
-                with db.engine.connect() as conn:
-                    conn.execute(stmt, {"Acode": Acode, "Aname": Aname, "Genre": genre, "Records": records, "Label": label})
-                
+                query=Artistdata(Acode=Acode,Aname=Aname,genre=genre,records=records,label=label)
+                db.session.add(query)
+                db.session.commit()
                 flash("Data is Added", "primary")
+                return redirect('/addartistinfo')
             else:
                 flash("Artist Code does not exist", "warning")
-             
+                return redirect('/addartistinfo')
 
         return render_template("artistdata.html", postsdata=postsdata)  
     
@@ -294,10 +297,13 @@ def aedit(id):
             records = request.form.get('Records')
             label = request.form.get('Label')
             Acode = Acode.upper()
-            stmt = text("UPDATE artistdata SET Acode=:Acode, Aname=:Aname, Genre=:genre, Records=:records, Label=:label WHERE id=:id")
-            with db.engine.connect() as conn:
-                conn.execute(stmt, {"Acode": Acode, "Aname": Aname, "genre": genre, "records": records, "label": label, "id": id})
-
+            post=Artistdata.query.filter_by(id=id).first()
+            post.Acode=Acode
+            post.Aname=Aname
+            post.genre=genre
+            post.records=records
+            post.label=label
+            db.session.commit()
             flash("Slot Updated", "danger")
             return render_template("/addartistinfo")
 
@@ -309,10 +315,8 @@ def aedit(id):
 @app.route("/adelete/<string:id>", methods=['POST','GET'])
 @login_required
 def adelete(id):
-    stmt = text("DELETE FROM artistdata WHERE id=:id")
-    with db.engine.connect() as conn:
-        conn.execute(stmt, {"id": id})
-
+    post=Artistdata.query.filter_by(id=id).first()
+    db.session.delete(post)
     flash("Date Deleted", "danger")
     return redirect("/addartistinfo")
 
